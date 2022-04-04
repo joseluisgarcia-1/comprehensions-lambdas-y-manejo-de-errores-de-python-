5eecd03d161667d8de877ee4e548f8ecda45e236
joseluisgarcia-1 <jlgarcia002@misena.edu.co>
2022-04-03 21:26:57 -0500

    apuntes hasta la clase 9

--- clase9_dictionary_comprehensions.py
+++ clase9_dictionary_comprehensions.py
@@ -9,10 +9,21 @@ def run():
     print(my_dict)
     print("***números impares***")
     my_dict2 ={}
-    for i in range(1,101):
+    for i in range(1,11):
         if i % 3 != 0:
             my_dict2[i] = i**3
     print(my_dict2)
+    """
+    dict comprehensions
+    """
+    print("números impares con dict comprehension")
+    """
+    - para cada i en el rango de 1 a 11 voy a guardar a i como llave y a i elevado al cubo como valor, solamente si modulo 3 es distinto de 0
+    - en otras palabras:
+        para cada número de 1 a 11 voy a guardar ese número como llave y ese número elevado al cubo como valor si ese número no es divisible entre 3
+    """
+    num = {i: i**3 for i in range (1,11) if i % 3 != 0}
+    print(num)
 
 if __name__ == '__main__':
     run()
